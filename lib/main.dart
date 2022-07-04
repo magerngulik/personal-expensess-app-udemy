3b47eaedf2fe59ad913c636ea27bffed0b18f52c
magerngulik <magerngulik@gmail.com>
2022-07-04 23:40:31 +0700

    85 adding input text/ text field

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyHomePage extends StatelessWidget {
           title: Text('Flutter App'),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Container(
@@ -30,6 +30,25 @@ class MyHomePage extends StatelessWidget {
                 color: Colors.blue,
                 child: Text('CHART!'),
                 elevation: 5,
+              ),
+            ),
+            Card(
+              elevation: 5,
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    TextField(
+                        decoration: InputDecoration(label: Text('Title'))),
+                    TextField(
+                        decoration: InputDecoration(label: Text('Amount'))),
+                    FlatButton(
+                        textColor: Colors.purple,
+                        onPressed: () {},
+                        child: Text('Add The Transaction')),
+                  ],
+                ),
               ),
             ),
             Column(
